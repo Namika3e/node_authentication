0eba595cfbc035340b1fbef1e46892b73dd5c09c
Namika3e <amobedac@gmail.com>
2024-11-17 19:15:15 +0100

    Added hosted netlify to cors

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     credentials: true,
     // methods: ["GET", "POST", "DELETE", "PUT"],
-    origin: ["http://localhost:5173", "http://localhost:8080"] 
+    origin: ["http://localhost:5173", "http://localhost:8080", "https://havestav1.netlify.app"] 
   })
 );
 
